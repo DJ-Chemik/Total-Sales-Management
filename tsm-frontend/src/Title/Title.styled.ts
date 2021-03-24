81061e060efcfd13a8d46b02fd8a4d3105026fff
Chemik <dj-chemik@wp.pl>
2021-03-24 22:11:11 +0100

    Add improvements to logo

--- tsm-frontend/src/Title/Title.styled.ts
+++ tsm-frontend/src/Title/Title.styled.ts
@@ -5,9 +5,16 @@ export const TitleWrapper = styled.div`
   font-family: 'TargetExpand';
   color: ${colors.REACT_CYAN};
   font-size: 3vw;
-  cursor: pointer;
+  margin-bottom: 1rem;
+  cursor: context-menu;
+  user-select: none;
 
   &:hover {
-    color: ${colors.NEON_MAGENTA_DARKER};
+    color: ${colors.NEON_MAGENTA_DARKER_2};
+  }
+
+  @media (max-height: 500px) {
+    font-size: 5vmin;
+    margin-bottom: 0.5rem;
   }
 `;
--- tsm-frontend/src/globalStyle.ts
+++ tsm-frontend/src/globalStyle.ts
@@ -3,7 +3,8 @@ export enum colors {
   DARK = '#282c34',
   BLACK = '#000000',
   NEON_MAGENTA = '#F39',
-  NEON_MAGENTA_DARKER = '#da0871',
+  NEON_MAGENTA_DARKER_1 = '#da0871',
+  NEON_MAGENTA_DARKER_2 = '#ca0561',
   RED = '#F00',
   BACKGROUND = '#000015',
 }
